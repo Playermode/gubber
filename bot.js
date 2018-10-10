d75678543bed85ac3cf0c353fe92e18a8ab06ad3
Playermode <36794669+Playermode@users.noreply.github.com>
2018-10-09 22:10:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,37 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const PREFIX = "!";
 
+
+var fortunes = [
+    "Woofs in yes",
+    "Woofs in no",
+    "Woofs in maybe",
+    "No thats a horrible idea why did you even ask that in the first place",
+    "WOOFS IN YES THATS THE BEST IDEA I HAVE HEARD IN MY LIFE",
+    "Lunax says yes so I say yes",
+    "Lunax says no so I say no"
+]; 
+  
+if (!message.content.startsWith(PREFIX)) return;
+
+  var args = message.content.substring(PREFIX.length).split(" ");
+
+  switch (args[0].toLocaleLowerCase()) {
+    case "8ball":
+      if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)])
+      else message.channel.sendMessage("Please ask me an actual question");
+      break;
+     case "ocean man":
+     message.channel.sendMessage("Barks in OCEAN MAN TAKE ME BY THE HAND LEAD ME TO THE LAND THAT YOU UNDERSTAND")
+     break;
+     case "gubber":
+          var gubber = new Discord.RichEmbed()
+              .setImage("https://cdn.discordapp.com/attachments/386610342077267968/488904676327161867/886A99AC44170632B97B47C35A61A18F8E3D10F6.png")
+         message.channel.sendEmbed(gubber);
+         break;         
+  }
+});
+
 client.on('ready', function() { 
     client.user.setGame("Trying to prevent my owner from falling");
     client.user.setUsername("Gubber");
@@ -10,6 +41,12 @@ client.on('ready', function() {
 
 client.on('message', message => {
     if (message.content === 'jay') {
+    	message.reply('*woofs in valid*');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'Jay') {
     	message.reply('*woofs in valid*');
   	}
 });
