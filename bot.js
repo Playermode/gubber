fc3f4bcec6da1c96310c3b943b6c472bae4e4349
Playermode <36794669+Playermode@users.noreply.github.com>
2018-10-09 21:34:17 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const PREFIX = "!";
 
-client.on('ready', () => {
-    console.log('I am ready!');
+client.on('ready', function() { 
+    client.user.setGame("Trying to prevent my owner from falling");
+    client.user.setUsername("Gubber");
+    console.log('Woof!');
 });
+
 
 client.on('message', message => {
     if (message.content === 'jay') {
