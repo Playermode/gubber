15938feb9cc9e9532d358422af5fbe5667beb85c
Playermode <36794669+Playermode@users.noreply.github.com>
2019-07-13 23:28:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -252,46 +252,6 @@ client.on("message", function(message) {
     message.react("🔥");
 }
 
-  if (message.content === "pensive") {
-    const gubberthegod = client.emojis.get("513427115362746407");                                 
-    message.react("513427115362746407");
-}
-
-  if (message.content === "listemojis") {
-    const emojiList = message.guild.emojis.map(e=>e.toString()).join(" ");
-    message.channel.send(emojiList);     
-}
-
-  if (message.content === "jeoy") {
-    const gubberthegod = client.emojis.get("514977702755696656");                                 
-    message.react("514977702755696656");
-}
-
-  if (message.content === "Jeoy") {
-    const gubberthegod = client.emojis.get("514977702755696656");                                 
-    message.react("514977702755696656");
-}
-
-  if (message.content === "JEOY") {
-    const gubberthegod = client.emojis.get("514977702755696656");                                 
-    message.react("514977702755696656");
-}              
-
-  if (message.content === "jEoy") {
-    const gubberthegod = client.emojis.get("514977702755696656");                                 
-    message.react("514977702755696656");
-}
-
-  if (message.content === "jeOy") {
-    const gubberthegod = client.emojis.get("514977702755696656");                                 
-    message.react("514977702755696656");
-}    
-
-  if (message.content === "jeoY") {
-    const gubberthegod = client.emojis.get("514977702755696656");                                 
-    message.react("514977702755696656");
-}  
-
   if (!message.content.startsWith(PREFIX)) return;
 
   var args = message.content.substring(PREFIX.length).split(" ");
